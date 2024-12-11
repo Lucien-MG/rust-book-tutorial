a56cae292bf8bfcc052389834f64712684f7dbe9
Lucien Martin-Gaffé <lucien.martingaffe@gmail.com>
2024-12-11 19:04:58 +0100

    Refactoring, add an args parsing function.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,16 +3,25 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
 
-    let file_path = match args.get(1) {
-        Some(file_path) => file_path,
-        None => "No file path given.",
-    };
+    let config = parse_config(&args);
 
-    println!("File: {file_path}");
+    println!("File: {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Error while reading the file.");
+    let contents = fs::read_to_string(config.file_path).expect("Error while reading the file.");
 
     println!("Content: {contents}");
 }
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &Vec<String>) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
+}
+
