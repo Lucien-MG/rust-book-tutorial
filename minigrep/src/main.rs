94018677f67ac455bfaf3689d3e0286c5e2cab0e
Lucien Martin-Gaffé <lucien.martingaffe@gmail.com>
2024-12-11 18:45:40 +0100

    Add the ability to read file.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,18 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    dbg!(&args);
+
+    let file_path = match args.get(1) {
+        Some(file_path) => file_path,
+        None => "No file path given.",
+    };
+
+    println!("File: {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Error while reading the file.");
+
+    println!("Content: {contents}");
 }
